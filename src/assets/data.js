35d18031ac008cb6a5ec20d3ffb02ab02f88cea0
Ebin Soby <mailebinsoby@gmail.com>
2021-12-07 01:33:26 -0500

    Setup JSON Server

--- src/assets/data.js
+++ src/assets/data.js
@@ -4,16 +4,16 @@ let notes = [
         "body": "For today\n\n-Go to Gym\n- Work\n- Grocery",
         "updated": "2021-07-14T13:49:02.078653Z",
     },
-    {
-        "id": 2,
-        "body": "Meet Willy \n\n- Repair Car\n- Clean Room",
-        "updated": "2021-07-13T20:43:18.550058Z",
-    },
-    {
-        "id": 3,
-        "body": "Build a React Project",
-        "updated": "2021-07-13T19:46:12.187306Z",
-    }
+    // {
+    //     "id": 2,
+    //     "body": "Meet Willy \n\n- Repair Car\n- Clean Room",
+    //     "updated": "2021-07-13T20:43:18.550058Z",
+    // },
+    // {
+    //     "id": 3,
+    //     "body": "Build a React Project",
+    //     "updated": "2021-07-13T19:46:12.187306Z",
+    // }
 ]
 
 
