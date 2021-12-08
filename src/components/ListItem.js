bde8516c3201b5dabc8e743098982b847a4fd0be
Ebin Soby <mailebinsoby@gmail.com>
2021-12-08 15:06:43 -0500

    App fully built 1.0

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,11 +1,34 @@
  import React from 'react'
  import { Link } from 'react-router-dom'
 
+ let getTime = (note)=>{
+     return new Date(note.updated).toLocaleDateString();
+ }
+
+ const getTitle = (note) =>{
+     const title = note.body.split("\n")[0];
+     if(title.length > 45)
+     return title.slice(0,45);
+     return title
+ }
+ const getContent = (note) => {
+     let title = getTitle(note);
+     let content = note.body.replaceAll("\n",'');
+     content = note.body.replaceAll(title,'');
+     if(content.length > 45){
+         return content.slice(0,45)+"..."
+     } else {
+        return content
+     }
+     
+ }
+
  const ListItem = ({note}) => {
      return (
          <div className="notes-list-item" >
              <Link to={`/note/${note.id}`} >
-             <h2>{note.body}</h2>
+             <h2>{getTitle(note)}</h2>
+             <p><span>{getTime(note)}</span>{getContent(note)}</p>
              </Link>
          </div>
      )
--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,22 +1,86 @@
-import React from "react";
-import notes from "../assets/data";
-import { ReactComponent as ArrowLeft } from "../assets/arrow-left.svg";
+import React, { useEffect, useState } from "react";
+// import notes from "../assets/data";
+import { ReactComponent as ArrowLeft } from "../assets/arrow-left-chevron.svg";
 import { Link } from "react-router-dom";
 
-function NotePage({ match }) {
+function NotePage({ match, history }) {
   let noteId = match.params.id;
+  let [note, setNote] = useState([]);
+  useEffect(() => {
+    // eslint-disable-next-line
+    getNote()}, [noteId]);
+
+  let getNote = async () => {
+    if (noteId === "new") return;
+    let response = await fetch(`http://localhost:5000/notes/${noteId}`);
+    let data = await response.json();
+    setNote(data);
+  };
+
+  let createNote = async () => {
+    await fetch(`http://localhost:5000/notes`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ...note, updated: new Date() }),
+    });
+  };
+
+  let updateNote = async () => {
+    await fetch(`http://localhost:5000/notes/${noteId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ ...note, updated: new Date() }),
+    });
+  };
+
+  let deleteNode = async () => {
+    await fetch(`http://localhost:5000/notes/${noteId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(note),
+    });
+    history.push("/");
+  };
+
+  let handleSubmit = () => {
+    if (noteId !== "new" && !note.body) {
+      deleteNode();
+    } else if (noteId !== "new") {
+      updateNote();
+    } else if (noteId === "new" && note !== null){
+      createNote();
+    }
+    history.push("/");
+  };
+
   // eslint-disable-next-line
-  let note = notes.find((note) => note.id == noteId);
+  // let note = notes.find((note) => note.id == noteId);
   return (
     <div className="note">
       <div className="note-header">
         <h3>
           <Link to={"/"}>
-            <ArrowLeft />
+            <ArrowLeft onClick={handleSubmit} />
           </Link>
         </h3>
+        {noteId !== "new" ? (
+          <button onClick={deleteNode}>Delete</button>
+        ) : (
+          <button onClick ={handleSubmit} >Done</button>
+        )}
       </div>
-      <textarea value={note?.body}></textarea>
+      <textarea
+        onChange={(e) => {
+          setNote({ ...note, body: e.target.value });
+        }}
+        value={note?.body}
+      ></textarea>
     </div>
   );
 }
